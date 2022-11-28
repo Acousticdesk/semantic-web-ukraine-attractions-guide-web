fb4adca54f29d5f6bdb56ee5166a779bf99c34fe
Andrii Kicha <akicha@star.global>
2022-11-28 18:53:50 +0200

    add attractions view

--- src/steps/ViewAttractions.component.tsx
+++ src/steps/ViewAttractions.component.tsx
@@ -1,10 +1,30 @@
-import { Text, Button, Spinner } from "@chakra-ui/react";
+import {
+  Text,
+  Button,
+  Spinner,
+  Card,
+  CardBody,
+  Stack,
+  Heading,
+  Image,
+  Divider,
+  CardFooter,
+  ButtonGroup,
+  Box,
+} from "@chakra-ui/react";
 import { useEffect, useState, useContext } from "react";
 import { API_BASE_URL } from "../const";
 import { FormContext } from "../logic/form";
 
+interface Attraction {
+  thumbnail: string;
+  label: string;
+  details: string[];
+  description: string;
+}
+
 export const ViewAttractionsComponent = () => {
-  const [attractions, setAttractions] = useState([]);
+  const [attractions, setAttractions] = useState<Attraction[]>([]);
   const { form } = useContext(FormContext);
 
   useEffect(() => {
@@ -25,17 +45,61 @@ export const ViewAttractionsComponent = () => {
     <div className="SelectCategoryComponent">
       <Text mb={2}>Рекомендації щодо обраних Вами критеріїв:</Text>
       {attractions && attractions.length ? (
-        <ul>
-          {attractions.map((a) => {
-            return <li key={a}>{a}</li>;
+        <Box>
+          {attractions.map(({ thumbnail, label, description, details }) => {
+            return (
+              <Card key={label}>
+                <CardBody>
+                  <Image src={thumbnail} alt={label} borderRadius="lg" />
+                  <Stack mt="6" spacing="3">
+                    <Heading size="md">{label}</Heading>
+                    <Text>{description}</Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <Button
+                      variant="ghost"
+                      colorScheme="blue"
+                      onClick={(e) => {
+                        (e.target as HTMLButtonElement).hidden = true;
+                        const element = document.getElementById(
+                          `details_${label}`
+                        );
+
+                        if (element) {
+                          element.removeAttribute("hidden");
+                        }
+                      }}
+                    >
+                      Більше Інформації
+                    </Button>
+                    <div hidden id={`details_${label}`}>
+                      <ul>
+                        {details &&
+                          details.length &&
+                          details.map((d) => (
+                            <li>
+                              <a href={d} key={d} target="_blank">
+                                {d}
+                              </a>
+                            </li>
+                          ))}
+                      </ul>
+                    </div>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            );
           })}
-        </ul>
+        </Box>
       ) : (
         <Spinner />
       )}
-      <div>
+      <Box mt={8}>
         <Button onClick={handleFinalize}>Спочатку</Button>
-      </div>
+      </Box>
     </div>
   );
 };
