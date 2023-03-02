7525847f4bc61cdeaa840e7d3583f91a5c18f4dc
Andrii Kicha <akicha@star.global>
2023-03-02 12:45:57 +0100

    squash

--- src/regions/utils/index.ts
+++ src/regions/utils/index.ts
@@ -1,2 +1,2 @@
 export const trimRegionNamespace = (region: string) =>
-  region.replace("http://dbpedia.org/resource/", "").replace("_Oblast", "");
+  region.replace("http://dbpedia.org/resource/", "");
